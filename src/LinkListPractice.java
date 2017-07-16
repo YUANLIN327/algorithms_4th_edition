92b371cd0ebcf99f05847a7dc3e8b0c232b4899a
Lin Yuan <yuanlin.dalian@gmail.com>
2017-07-16 19:10:01 -0400

    Reverse a stack given the first node.

--- src/LinkListPractice.java
+++ src/LinkListPractice.java
@@ -4,6 +4,14 @@ public class LinkListPractice<Item> {
     private class Node {
         Item item;
         Node next;
+
+        public Node(Item i) {
+            this.item = i;
+        }
+
+        public String toString() {
+            return hashCode() + ":" + item;
+        }
     }
 
     static boolean find(Object o, LinkedList ll) {
@@ -27,8 +35,29 @@ public class LinkListPractice<Item> {
         }
     }
 
+    Node reverse(Node node) {
+        Node reverse = null;
+        Node first = node;
+        while (first != null) {
+            Node second = first.next;
+            first.next = reverse;
+            reverse = first;
+            first = second;
+        }
+        return reverse;
+    }
 
+    Node reverseRecrusively(Node first) {
+        if (first == null) return null;
+        if (first.next == null) return first;
+        Node nextNode = first.next;
+        first.next = null;
+        Node rest = reverseRecrusively(nextNode);
+        rest.next = first;
+        return rest;
+    }
 
     public static void main(String[] args) {
+        LinkListPractice lp = new LinkListPractice();
     }
 }
