de62e013428ecc220fd1f4c2e1affc3816caa1b0
Lin Yuan <yuanlin.dalian@gmail.com>
2017-07-27 02:14:57 -0400

    MergeBU enhancement.

--- src/Chap2_Merge_Sort/MergeBU.java
+++ src/Chap2_Merge_Sort/MergeBU.java
@@ -7,13 +7,11 @@ public class MergeBU {
     public static void sort(Comparable[] arr) {
         Comparable[] aux = new Comparable[arr.length];
         int size = 1;
-        while (size < arr.length) {
-            for (int i = 0; i < arr.length; i += (size + size)) {
-                int lo = i;
-                int hi = Math.min(lo + size + size - 1, arr.length - 1);
-                if (lo >= hi) continue;
+        int length = arr.length;
+        while (size < length) {
+            for (int lo = 0; lo < length - size; lo += (size + size)) {
+                int hi = Math.min(lo + size + size - 1, length - 1);
                 int mid = lo + size - 1;
-                if (mid >= hi) continue;
                 if (MergeSort.less(arr[mid], arr[mid + 1])) continue;
                 MergeSort.merge(arr, aux, lo, mid, hi);
             }
